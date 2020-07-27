f614fad86b92710fc1a559adf90b8842d3717575
aposipa <andrew.posipanka@gmail.com>
2020-07-27 15:35:06 -0400

    initial grid setup.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import produce from 'immer';
 import './App.css';
 
+const numRows = 50;
+const numCols = 50;
+
+
+
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0))
+    }
+    return rows
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: 'grid',
+      gridTemplateColumns: `repeat(${numCols}, 20px)`
+    }}>
+      {grid.map((rows, i) => 
+      rows.map((col, k) => 
+      <div 
+      key={`${i}-${k}`}
+      onClick={() => {
+        const newGrid = produce(grid, gridCopy => {
+          gridCopy[i][k] = grid[i][k] ? 0 : 1;
+        });
+        setGrid(newGrid);
+      }}
+      style = {{ 
+        width: 20, 
+        height: 20, 
+        backgroundColor: grid[i][k] ? 'black': undefined, 
+        border: 'solid 1px black'
+      }} 
+      />)
+      )}
     </div>
   );
-}
+};
 
 export default App;
