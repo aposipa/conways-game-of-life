47e5d89d8960c1a54eaff2336a24133d5e9f6f58
aposipa <andrew.posipanka@gmail.com>
2020-07-30 17:13:18 -0400

    added styling.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useCallback, useRef } from 'react';
-import produce from 'immer';
+import produce, { current } from 'immer';
 import './App.css';
 
+
 const numRows = 25;
-const numCols = 25;
+const numCols = 35;
+
 
 const operations = [
   [0, 1],
@@ -20,6 +22,13 @@ const operations = [
 function App() {
   const [time, setTime] = useState(100);
   
+  // const [generation, setGeneration] = useState(0);
+
+  // const getGeneration = () => {
+  //   setGeneration += 1
+  //   return setGeneration;
+  // }
+
   // clears current grid
   const createEmptyGrid = () => {
     const rows = [];
@@ -28,11 +37,11 @@ function App() {
     }
     return rows
   }
-  
+
   const [grid, setGrid] = useState(() => {
     return createEmptyGrid()
   });
-  
+
   const [running, setRunning] = useState(false);
   
   const runningRef = useRef(running);
@@ -66,6 +75,8 @@ function App() {
       });
     });
     setTimeout(runSimulation, time);
+    // getGeneration();
+    // console.log(generation)
   }, [time])
   
   const handleTimeChange = (e) => {
@@ -102,56 +113,20 @@ function App() {
         }
       });
     });
-
   }, [])
 
   return (
     <>
-    <button
-    onClick={() => {
-      setRunning(!running);
-      runningRef.current = true;
-      runSimulation();
-    }}
-    >
-    Start
-    </button>
-    <button
-    onClick={() => {
-      setRunning(!running);
-      runningRef.current = false;
-    }}
-    >
-    Stop
-    </button>
-    <button onClick={() => {
-      setGrid(createEmptyGrid());
-    }}>Clear</button>
-    <button onClick={() => {
-      const rows = [];
-      // randomizes grid 50/50 either alive or dead for each space.
-      for (let i = 0; i < numRows; i++) {
-        rows.push(Array.from(Array(numCols), () => Math.random() > .5 ? 1 : 0));
-      }
-
-      setGrid(rows);
-    }}>Randomize Grid</button>
-    <button onClick={() => {
-      setRunning(!running);
-      runningRef.current = true;
-      justOneSimulation();
-    }}>Next Generation</button>
-    <form onSubmit={handleSubmit}>
+    <div className="game-display">
+    <header>
+      <h1>
+      Conway's Game of Life
+      </h1>
+    </header>
+    <div className="left-side">
       <label>
-        Time Interval:
-        <input
-        value={time}
-        type="number"
-        onChange={handleTimeChange}
-        />
-        msec
+        # Generation: 0
       </label>
-    </form>
     <div style={{
       display: 'grid',
       gridTemplateColumns: `repeat(${numCols}, 20px)`
@@ -177,6 +152,80 @@ function App() {
       }} 
       />)
       )}
+    </div>
+    <div className="buttons">
+    <button className="button"
+    onClick={() => {
+      setRunning(!running);
+      runningRef.current = true;
+      runSimulation();
+    }}
+    >
+    Start
+    </button>
+    <button className="button"
+    onClick={() => {
+      setRunning(!running);
+      runningRef.current = false;
+    }}
+    >
+    Stop
+    </button>
+    <button  className="button"
+    onClick={() => {
+      setGrid(createEmptyGrid());
+    }}>Clear</button>
+    <button className="button"
+    onClick={() => {
+      const rows = [];
+      // randomizes grid 50/50 either alive or dead for each space.
+      for (let i = 0; i < numRows; i++) {
+        rows.push(Array.from(Array(numCols), () => Math.random() > .5 ? 1 : 0));
+      }
+
+      setGrid(rows);
+    }}>Randomize Grid</button>
+    <button className="button"
+    onClick={() => {
+      setRunning(!running)
+      runningRef.current = true;
+      justOneSimulation();
+
+    }}>Next Generation</button>
+    <form className="time-interval"onSubmit={handleSubmit}>
+      <label>
+        Time Interval: 
+        <input
+        value={time}
+        type="number"
+        onChange={handleTimeChange}
+        />
+        msec
+      </label>
+    </form>
+    </div>
+    </div>
+    <div className="game-text">
+      <h2>Rules:</h2>
+      <p>In the Game of Life, these rules examine each cell of the grid. For each cell, it counts that cell's eight neighbors (up, down, left, right, and diagonals), and then act on that result.
+      <ul>
+      <li>If the cell is alive and has 2 or 3 neighbors, then it remains alive. Else it dies.</li>
+      <li>If the cell is dead and has exactly 3 neighbors, then it comes to life. Else if remains dead.</li>
+      </ul>
+      From those two rules, many types of "creatures" can be created that move around the "landscape".
+      Note: cells that are off the edge of the grid are typically assumed to be dead. </p>
+      <h2>Interactive Rules:</h2>
+      <ul>
+      <li>You can customize the grid to your liking while the game is <u><b>not</b></u> running.  Simply click on a square to make it alive, or dead.</li>
+      <li>The 'Randomize Grid' button will generate a random grid to start with.</li>
+      <li>The 'Next Generation' button will manually progress the game one generation at a time.</li>
+      <li>You can set the runtime of the game via the 'Time Interval' by inputing your own time.  Time is in milliseconds(1000 msec = 1 sec).</li>
+      </ul>
+    </div>
+    <footer className="alg-text">
+      <h2>About this Algorithm:</h2>
+      <p>In computability theory, a system of data-manipulation rules (such as a computer's instruction set, a programming language, or a cellular automaton) is said to be Turing-complete or computationally universal if it can be used to simulate any Turing machine. This means that this system is able to recognize or decide other data-manipulation rule sets. Turing completeness is used as a way to express the power of such a data-manipulation rule set. Virtually all programming languages today are Turing-complete. The concept is named after English mathematician and computer scientist Alan Turing.</p>
+    </footer>
     </div>
     </>
   );
